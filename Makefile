f1d5a4acc829563c2d06b2fc37b01372e971e5c8
Damián Silvani <munshkr@gmail.com>
2013-09-14 17:24:49 -0300

    Player can shoot lasers, w00t!

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC	= ../../bin/lcc -Wa-l -Wl-m -Wl-j
 EMULATOR = VisualBoyAdvance
-EMULATOR_FLAGS = -4
+EMULATOR_FLAGS = -4 --ifb-motion-blur
 BIN	= shmup.gb
 
 all:	$(BIN)
 
 run:	$(BIN)
-	$(EMULATOR) $(BIN)
+	$(EMULATOR) $(EMULATOR_FLAGS) $(BIN)
 
 make.bat: Makefile
 	echo "REM Automatically generated from Makefile" > make.bat
--- shmup.c
+++ shmup.c
@@ -3,56 +3,102 @@
 #include "sprites.h"
 #include "sprites.c"
 
-#define SP_NULL 0
-#define SP_SHIP 1
+#define SP_NULL     0
+#define SP_SHIP     1
+#define SP_LASER    2
 
-int n;
+#define MAX_LASERS  8
+
+int i;
 UBYTE key;
 
-typedef struct {
-    int x;
-    int y;
-} player_t;
+BOOLEAN pshot;
+int px, py;
 
-player_t player;
+BOOLEAN ls[MAX_LASERS];
+int lx[MAX_LASERS];
+int ly[MAX_LASERS];
+
 
 void init_screen() {
     SPRITES_8x8;
 
-    set_sprite_data(0, 2, sprites);
+    set_sprite_data(0, 3, sprites);
 
     // clear OAM
-    for (n = 0; n < 40; n++) {
-        set_sprite_tile(n, SP_NULL);
+    for (i = 0; i < 40; i++) {
+        set_sprite_tile(i, SP_NULL);
     }
 
     SHOW_SPRITES;
 }
 
-void init_player() {
-    set_sprite_tile(0, SP_SHIP);
-    player.x = 84;
-    player.y = 140;
-    //move_sprite(0, 84, 140);
+void init_lasers() {
+    for (i = 0; i < MAX_LASERS; i++) {
+        set_sprite_tile(i + 1, SP_LASER);
+
+        ls[i] = FALSE;
+        lx[i] = 0;
+        ly[i] = 0;
+    }
 }
 
-void move_player() {
+void init_player() {
+    set_sprite_tile(0, SP_SHIP);
+
+    px = 84;
+    py = 140;
+}
+
+void handle_input() {
     key = joypad();
 
     if (key & J_LEFT) {
-        if (player.x > 8) player.x--;
+        if (px > 8) px--;
     } else if (key & J_RIGHT) {
-        if (player.x < 160) player.x++;
+        if (px < 160) px++;
     }
 
     if (key & J_UP) {
-        if (player.y > 16) player.y--;
+        if (py > 16) py--;
     } else if (key & J_DOWN) {
-        if (player.y < 151) player.y++;
+        if (py < 151) py++;
     }
 
-    move_sprite(0, player.x, player.y);
+    if (key & J_A && !pshot) {
+        for (i = 0; i < MAX_LASERS; i++) {
+            if (!ls[i]) {
+                ls[i] = TRUE; lx[i] = px; ly[i] = py;
+                break;
+            }
+        }
+        pshot = TRUE;
+    } else if (!(key & J_A)) {
+        pshot = FALSE;
+    }
 }
+
+void move_lasers() {
+    for (i = 0; i < MAX_LASERS; i++) {
+        if (ls[i]) {
+            ly[i] -= 2;
+            if (ly[i] <= 0) {
+                ls[i] = FALSE;
+            }
+        }
+    }
+}
+
+void update_sprites() {
+    move_sprite(0, px, py);
+
+    for (i = 0; i < MAX_LASERS; i++) {
+        if (ls[i]) {
+            move_sprite(i + 1, lx[i], ly[i]);
+        }
+    }
+}
+
 
 void main() {
     disable_interrupts();
@@ -60,13 +106,18 @@ void main() {
 
     init_screen();
     init_player();
+    init_lasers();
 
     DISPLAY_ON;
     enable_interrupts();
 
     while (1) {
         wait_vbl_done();
-        move_player();
+        handle_input();
+
+        move_lasers();
+
+        update_sprites();
     }
 }
 
